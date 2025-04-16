d7c1197944735385bab2f8b4db7018cd934097db
inidaoaa <apuddmz@gmail.com>
2025-04-16 15:34:30 +0700

    maintance4

---
+++ lib/app/data/data_barang_response.dart
@@ -0,0 +1,169 @@
+class DataBarangResponse {
+  String? status;
+  String? message;
+  Data? data;
+
+  DataBarangResponse({this.status, this.message, this.data});
+
+  DataBarangResponse.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    message = json['message'];
+    data = json['data'] != null ? new Data.fromJson(json['data']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['status'] = status;
+    data['message'] = message;
+    if (this.data != null) {
+      data['data'] = this.data!.toJson();
+    }
+    return data;
+  }
+}
+
+class Data {
+  int? currentPage;
+  List<Data>? data;
+  String? firstPageUrl;
+  int? from;
+  int? lastPage;
+  String? lastPageUrl;
+  List<Links>? links;
+  Null? nextPageUrl;
+  String? path;
+  int? perPage;
+  Null? prevPageUrl;
+  int? to;
+  int? total;
+
+  Data(
+      {this.currentPage,
+      this.data,
+      this.firstPageUrl,
+      this.from,
+      this.lastPage,
+      this.lastPageUrl,
+      this.links,
+      this.nextPageUrl,
+      this.path,
+      this.perPage,
+      this.prevPageUrl,
+      this.to,
+      this.total});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    currentPage = json['current_page'];
+    if (json['data'] != null) {
+      data = <Data>[];
+      json['data'].forEach((v) {
+        data!.add(new Data.fromJson(v));
+      });
+    }
+    firstPageUrl = json['first_page_url'];
+    from = json['from'];
+    lastPage = json['last_page'];
+    lastPageUrl = json['last_page_url'];
+    if (json['links'] != null) {
+      links = <Links>[];
+      json['links'].forEach((v) {
+        links!.add(new Links.fromJson(v));
+      });
+    }
+    nextPageUrl = json['next_page_url'];
+    path = json['path'];
+    perPage = json['per_page'];
+    prevPageUrl = json['prev_page_url'];
+    to = json['to'];
+    total = json['total'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['current_page'] = currentPage;
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    data['first_page_url'] = firstPageUrl;
+    data['from'] = from;
+    data['last_page'] = lastPage;
+    data['last_page_url'] = lastPageUrl;
+    if (links != null) {
+      data['links'] = links!.map((v) => v.toJson()).toList();
+    }
+    data['next_page_url'] = nextPageUrl;
+    data['path'] = path;
+    data['per_page'] = perPage;
+    data['prev_page_url'] = prevPageUrl;
+    data['to'] = to;
+    data['total'] = total;
+    return data;
+  }
+}
+
+class DataBarang {
+  int? id;
+  String? namaBarang;
+  String? jenisBarang;
+  String? merek;
+  int? jumlah;
+  String? createdAt;
+  String? updatedAt;
+  String? pembeliansSumJumlah;
+
+  DataBarang(
+      {this.id,
+      this.namaBarang,
+      this.jenisBarang,
+      this.merek,
+      this.jumlah,
+      this.createdAt,
+      this.updatedAt,
+      this.pembeliansSumJumlah});
+
+  DataBarang.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    namaBarang = json['nama_barang'];
+    jenisBarang = json['jenis_barang'];
+    merek = json['merek'];
+    jumlah = json['jumlah'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+    pembeliansSumJumlah = json['pembelians_sum_jumlah'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['id'] = id;
+    data['nama_barang'] = namaBarang;
+    data['jenis_barang'] = jenisBarang;
+    data['merek'] = merek;
+    data['jumlah'] = jumlah;
+    data['created_at'] = createdAt;
+    data['updated_at'] = updatedAt;
+    data['pembelians_sum_jumlah'] = pembeliansSumJumlah;
+    return data;
+  }
+}
+
+class Links {
+  String? url;
+  String? label;
+  bool? active;
+
+  Links({this.url, this.label, this.active});
+
+  Links.fromJson(Map<String, dynamic> json) {
+    url = json['url'];
+    label = json['label'];
+    active = json['active'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['url'] = url;
+    data['label'] = label;
+    data['active'] = active;
+    return data;
+  }
+}
--- lib/app/modules/dashboard/views/data_barang_view.dart
+++ lib/app/modules/dashboard/views/data_barang_view.dart
@@ -1,7 +1,10 @@
+import 'package:daoajikom/app/data/data_barang_response.dart';
 import 'package:daoajikom/app/modules/databarang/views/add_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:daoajikom/app/modules/databarang/controllers/databarang_controller.dart';
+import 'package:lottie/lottie.dart';
+import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
 class DataBarangView extends StatelessWidget {
   const DataBarangView({super.key});
@@ -10,23 +13,128 @@ class DataBarangView extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.put(DataBarangController());
 
-    return Scaffold(
+        return Scaffold(
       appBar: AppBar(
-        title: const Text('Data Barang'),
+        // Membuat AppBar dengan judul "Event List"
+        title: const Text('Informasi'),
+        centerTitle: true, // Menjadikan judul di tengah AppBar
       ),
-      body: const Center(
-        child: Text('Halaman Data Barang'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Get.to(() => const AddView())?.then((_) {
-            controller.DataBarang(); // Refresh data setelah kembali
-          });
-        },
-        child: const Icon(Icons.add),
+      body: Padding(
+        padding:
+            const EdgeInsets.all(16.0), // Memberi padding di sekitar konten
+        child: FutureBuilder<DataBarangResponse>(
+          // Mengambil data event melalui fungsi getEvent dari controller
+          future: controller.getInformation(),
+          builder: (context, snapshot) {
+            // Jika data sedang dimuat, tampilkan animasi loading
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: Lottie.network(
+                  // Menggunakan animasi Lottie untuk tampilan loading
+                  'https://gist.githubusercontent.com/olipiskandar/4f08ac098c81c32ebc02c55f5b11127b/raw/6e21dc500323da795e8b61b5558748b5c7885157/loading.json',
+                  repeat: true, // Animasi akan berulang terus-menerus
+                  width: MediaQuery.of(context).size.width /
+                      1, // Menyesuaikan lebar animasi
+                ),
+              );
+            }
+
+            if (!snapshot.hasData ||
+                snapshot.data == null ||
+                snapshot.data!.data == null) {
+              return const Center(child: Text("Tidak ada data"));
+            }
+
+            // Jika tidak ada data yang diterima, tampilkan pesan "Tidak ada data"
+            if (snapshot.data!.data!.isEmpty) {
+              return const Center(child: Text("Tidak ada data"));
+            }
+
+            return ListView.builder(
+              // Menentukan jumlah item berdasarkan panjang data data
+              itemCount: snapshot.data!.data!.length,
+              controller:
+                  scrollController, // Menggunakan ScrollController untuk ListView
+              shrinkWrap:
+                  true, // Membatasi ukuran ListView agar sesuai dengan konten
+              itemBuilder: (context, index) {
+                final item = snapshot.data!.data![index];
+                return ZoomTapAnimation(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment
+                        .start, // Menyusun elemen secara horizontal di kiri
+                    children: [
+                      // Menampilkan gambar event
+                      Image.network(
+                        item.image ?? 'default_image_url',
+                        fit: BoxFit
+                            .cover, // Menyesuaikan gambar dengan area tampilan
+                        height: 200,
+                        width: 500,
+                        errorBuilder: (context, error, stackTrace) {
+                          // Menangani error jika gambar tidak ditemukan
+                          return const SizedBox(
+                            height: 200,
+                            child: Center(
+                              child: Text('Image not found'),
+                            ),
+                          );
+                        },
+                      ),
+                      const SizedBox(height: 16), // Jarak antara elemen
+                      // Menampilkan judul event
+                      Text(
+                        snapshot.data!.data![index].title.toString(),
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight:
+                              FontWeight.bold, // Membuat teks menjadi tebal
+                        ),
+                      ),
+                      const SizedBox(height: 8), // Jarak antara elemen
+                      // Menampilkan deskripsi event
+                      Text(
+                        snapshot.data!.data![index].content.toString(),
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey, // Warna teks abu-abu
+                        ),
+                      ),
+                      // Garis pembatas antara item
+                      const Divider(
+                        height: 10, // Tinggi divider
+                      ),
+                      const SizedBox(height: 16), // Jarak setelah divider
+                      // Menampilkan nama kategori
+                      Text(
+                        'Kategori: ${item.category?.name ?? "Tidak diketahui"}',
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+
+                      // Menampilkan User ID
+                      Text(
+                        'Penulis: ${item.user?.name ?? "-"}',
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                      const Divider(height: 10),
+                      const SizedBox(height: 16),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
+
   
-  DataBarangController() {}
+
 }
